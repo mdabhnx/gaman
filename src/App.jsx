3e4143e1da419e281a45db4113c6dabd52707924
mdabhnx <mdabhnx+github@gmail.com>
2023-06-01 15:34:09 +0600

    Footer component added for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
