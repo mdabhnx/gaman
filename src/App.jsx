2c2e9ac280fa2c2e53e1c86e12aa6bed0fe7dbc2
mdabhnx <mdabhnx+github@gmail.com>
2023-06-15 15:48:20 +0600

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
 import Monetization from "./components/Monetization/Monetization";
 import Faq from "./components/Faq/Faq";
+import Steps from "./components/Steps/Steps";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <BrowserRouter>
         <Header />
         <Hero />
+        <Steps />
         <Faq />
         <Monetization />
         <Footer />
---
+++ src/components/Steps/Steps.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./steps.scss";
+
+const Steps = () => {
+  return (
+    <div className="steps-container">
+      <div className="steps-wrapper">
+        <div className="steps-contents">
+          <div className="steps-left-contents">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            aut reiciendis, eveniet possimus dicta voluptatibus sed iure labore
+            culpa nisi?
+          </div>
+          <div className="steps-right-contents"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Steps;
