4a1ef0cc60fb2db2a5fc57941da5c5b23e42b82c
mdabhnx <mdabhnx+github@gmail.com>
2023-06-16 12:23:23 +0600

    dynamic line color added

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -21,31 +21,73 @@ const Steps = () => {
 
             <div className="per-steps">
               <div className="step">
-                <div className="rank">
+                <div className="rank rank-active">
                   <div className="rank-no">1</div>
                 </div>
-                <div className="info"></div>
+                <div className="info">
+                  <div className="icon-div">
+                    <img
+                      src="/assets/awesome-user-plus.png"
+                      alt="awesome-user-plus"
+                    />
+                  </div>
+                  <div className="content">
+                    <div>Create your account</div>
+                  </div>
+                </div>
               </div>
 
               <div className="step">
                 <div className="rank">
                   <div className="rank-no">2</div>
                 </div>
-                <div className="info"></div>
+                <div className="info">
+                  <div className="icon-div">
+                    <img
+                      src="/assets/ionic-logo-game-controller-b.png"
+                      alt="awesome-user-plus"
+                    />
+                  </div>
+                  <div className="content">
+                    <div>Select a game</div>
+                  </div>
+                </div>
               </div>
 
               <div className="step">
                 <div className="rank">
                   <div className="rank-no">3</div>
                 </div>
-                <div className="info"></div>
+                <div className="info">
+                  <div className="icon-div">
+                    <img
+                      src="/assets/awesome-trophy.png"
+                      alt="awesome-trophy"
+                    />
+                  </div>
+                  <div className="content">
+                    <div>
+                      Create your first contests <br />
+                      or tournament.
+                    </div>
+                  </div>
+                </div>
               </div>
 
               <div className="step">
                 <div className="rank">
                   <div className="rank-no">4</div>
                 </div>
-                <div className="info"></div>
+                <div className="info">
+                  <div className="icon-div">
+                    <img src="/assets/awesome-share.png" alt="awesome-share" />
+                  </div>
+                  <div className="content">
+                    <div>
+                      Share the link with your audience and start earning
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
