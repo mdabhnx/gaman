0bec4048f91d0cf191ad666644cc9761986d6eac
mdabhnx <mdabhnx+github@gmail.com>
2023-06-19 14:53:01 +0600

    creator style updated for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
         <Header />
         <Hero />
         <Steps />
-        {/* <Creators /> */}
+        <Creators />
         {/* <Faq /> */}
         {/* <Monetization /> */}
         {/* <Footer /> */}
