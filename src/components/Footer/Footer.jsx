5072e9adb8f8f7b00288df166e55fb82e42d4a8c
mdabhnx <mdabhnx+github@gmail.com>
2023-06-20 15:09:51 +0600

    Footer finalized...

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,26 +5,69 @@ const Footer = () => {
   return (
     <>
       <div className="footer-container">
-        <div className="wrapper">
-          <div className="footer-content">
-            <div className="footer-logo">
-              <div className="logo-container">
-                <img src="/assets/logo.svg" alt="logo" />
+        <div>
+          <div className="wrapper">
+            <div className="footer-content">
+              <div className="footer-logo">
+                <div className="logo-container">
+                  <img src="/assets/logo.svg" alt="logo" />
+                </div>
+                <div className="logo-desc">Made with 🤍 in India</div>
               </div>
-              <div className="logo-desc">Made with 🤍 in India</div>
-            </div>
-            <div className="footer-navigation">
-              <div className="left-links">
-                <ul>
-                  <li>About us</li>
-                  <li>Privacy policy</li>
-                  <li>Terms & condition</li>
-                  <li>Careers</li>
-                  <li>Creator zone</li>
-                </ul>
+              <div className="footer-navigation">
+                <div className="left-links">
+                  <ul>
+                    <li>About us</li>
+                    <li>Privacy policy</li>
+                    <li>Terms & condition</li>
+                    <li>Careers</li>
+                    <li>Creator zone</li>
+                  </ul>
+                </div>
+                <div className="right-links">
+                  <div className="follow-us">
+                    <div className="name">Follow us on</div>
+                    <div className="icons">
+                      <img
+                        src="/assets/insta-icon.svg"
+                        alt="icons"
+                        lazy={"true"}
+                        srcSet=""
+                      />
+                      <img
+                        src="/assets/fb-icon.svg"
+                        alt="icons"
+                        lazy={"true"}
+                        srcSet=""
+                      />
+                      <img
+                        src="/assets/tweeter-icon.svg"
+                        alt="icons"
+                        lazy={"true"}
+                        srcSet=""
+                      />
+                      <img
+                        src="/assets/li-icon.svg"
+                        alt="icons"
+                        lazy={"true"}
+                        srcSet=""
+                      />
+                      <img
+                        src="/assets/yt-icon.svg"
+                        alt="icons"
+                        lazy={"true"}
+                        srcSet=""
+                      />
+                    </div>
+                  </div>
+                  <div className="contact-us">
+                    <div>support@gaman.in</div>
+                    <button>CONTACT US</button>
+                  </div>
+                </div>
               </div>
-              <div className="right-links">
-                <div className="follow-us">
+              <div className="follow-us-mobile">
+                <div className="fum-wrapper">
                   <div className="name">Follow us on</div>
                   <div className="icons">
                     <img
@@ -58,47 +101,6 @@ const Footer = () => {
                       srcSet=""
                     />
                   </div>
-                </div>
-                <div className="contact-us">
-                  <div>support@gaman.in</div>
-                  <button>CONTACT US</button>
-                </div>
-              </div>
-            </div>
-            <div className="follow-us-mobile">
-              <div className="fum-wrapper">
-                <div className="name">Follow us on</div>
-                <div className="icons">
-                  <img
-                    src="/assets/insta-icon.svg"
-                    alt="icons"
-                    lazy={"true"}
-                    srcSet=""
-                  />
-                  <img
-                    src="/assets/fb-icon.svg"
-                    alt="icons"
-                    lazy={"true"}
-                    srcSet=""
-                  />
-                  <img
-                    src="/assets/tweeter-icon.svg"
-                    alt="icons"
-                    lazy={"true"}
-                    srcSet=""
-                  />
-                  <img
-                    src="/assets/li-icon.svg"
-                    alt="icons"
-                    lazy={"true"}
-                    srcSet=""
-                  />
-                  <img
-                    src="/assets/yt-icon.svg"
-                    alt="icons"
-                    lazy={"true"}
-                    srcSet=""
-                  />
                 </div>
               </div>
             </div>
